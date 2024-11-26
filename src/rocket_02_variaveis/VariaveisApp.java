9c421bbc1490b92cb62ccdee09eb03d40143d05e
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-11-26 18:34:23 -0300

    aula variaveis

---
+++ src/rocket_02_variaveis/VariaveisApp.java
@@ -0,0 +1,11 @@
+package rocket_02_variaveis;
+
+public class VariaveisApp {
+
+    public static void main(String[] args) {
+        String Br = "Brasil";
+        double PI = 3.14;
+        final int ESTADOS_BRASILEIROS = 27;
+        final int ANO_2000 = 2000;
+    }
+}
