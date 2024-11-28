9ef31c8967bcb55eebebb8544a3ff69028472dc8
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-11-28 10:14:57 -0300

    adicionando encapsulamento

--- src/exercicios/Conta.java
+++ src/exercicios/Conta.java
@@ -1,37 +1,78 @@
 package exercicios;
 
 public class Conta {
-    public String numeroConta;
-    public String agencia;
-    public String nomeCliente;
-    public String dataNascimento;
-    public double Saldo;
+	private String numeroConta;
+	private String agencia;
+	private String nomeCliente;
+	private String dataNascimento;
+    private double saldo;
 
     public Conta(String conta, String agencia, String nome, String nascimento, double saldo){
         this.numeroConta = conta;
         this.agencia = agencia;
         this.nomeCliente = nome;
         this.dataNascimento = nascimento;
-        this.Saldo = saldo;
+        this.saldo = saldo;
     }
+    
+    public String getNumeroConta() {
+		return numeroConta;
+	}
 
-    public void Sacar(double valor){
-        if(valor > this.Saldo){
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void Sacar(double valor){
+        if(valor > this.saldo || this.saldo == 0){
             System.out.println("Saldo insuficiente");
         }else{
-            this.Saldo -= valor;
+            this.saldo -= valor;
             System.out.println("Saque efetuado!");
         }
     }
 
     public void ConsultarSaldo(){
-        System.out.println("O seu saldo atual é " + this.Saldo);
+        System.out.println("O seu saldo atual é " + this.saldo);
     }
 
     public void Transferir(Conta alvo, double valor){
-        if(this.Saldo >= 0 && this.Saldo >= valor){
-            this.Saldo -= valor;
-            alvo.Saldo += valor;
+        if(this.saldo >= 0 && this.saldo >= valor){
+            this.saldo -= valor;
+            alvo.saldo += valor;
             System.out.println("Transferência efetuada!");
         }else{
             System.out.println("Saldo insuficiente");
